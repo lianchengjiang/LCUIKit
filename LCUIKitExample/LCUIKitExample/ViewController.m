2cfa584c184a425c846c747730d73f71de77ebc2
jiangliancheng <1193244269@qq.com>
2016-04-09 15:07:50 +0800

    update example

--- LCUIKitExample/LCUIKitExample/ViewController.m
+++ LCUIKitExample/LCUIKitExample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+#import "UIButton+LCAlignment.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *label = ({
+        UIView *label = [[UIView alloc] init];
+        [self.view addSubview:label];
+        [label setBackgroundColor:[UIColor blueColor]];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.center.offset(0);
+            make.size.sizeOffset(CGSizeMake(100, 100));
+        }];
+        label;
+    });
+    
+    UILabel *label1 = ({
+        UILabel *label = [[UILabel alloc] init];
+        [self.view addSubview:label];
+        [label setBackgroundColor:[UIColor orangeColor]];
+        label.text = @"text";
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.left.offset(100);
+        }];
+        label;
+    });
+
+
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setBackgroundColor:[UIColor redColor]];
+    [btn setTitle:@"title" forState:UIControlStateNormal];
+//    [btn.titleLabel setFont:[UIFont systemFontOfSize:30]];
+    [btn.titleLabel setBackgroundColor:[UIColor greenColor]];
+    [btn setImage:[UIImage imageNamed:@"icon_facebook"] forState:UIControlStateNormal];
+    [label addSubview:btn];
+    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.top.offset(0);
+    }];
+    [btn lc_titleImageHorizontalAlignmentWithSpace:10];
+}
+
+- (void)injected
+{
+    NSLog(@"I've been injected: %@", self);
 }
 
 - (void)didReceiveMemoryWarning {
